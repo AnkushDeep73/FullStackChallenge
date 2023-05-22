b8392029f55f8e2257a27ef0b9a7c3b3212e453f
Ankush Deep <manchester7.united.ad@gmail.com>
2023-05-22 11:01:11 -0700

    Updating home page contents to include more detail and change Invoice actions to Borrower actions.

--- app/javascript/components/Home.jsx
+++ app/javascript/components/Home.jsx
@@ -5,34 +5,38 @@ export default () => (
   <div className="vw-100 vh-100 primary-color d-flex align-items-center justify-content-center">
     <div className="jumbotron jumbotron-fluid bg-transparent">
       <div className="container secondary-color">
-        <h1 className="display-4">Invoice Management</h1>
+        <h1 className="display-4">Lender Management Tool</h1>
         <p className="lead">
-          Management of Invoices of Borrowers. Invoices can be created, rejected, approved, purchased and closed.
+          Lender Management tool for Borrowers and their Invoices.
+          <br></br>
+          Using this tool, Borrowers can be created and their Invoices can be created, rejected, approved, purchased and closed.
           <br></br>
           <br></br>
           Here are the basics -
           <br></br>
-          i) Only created invoices can be approved or rejected.
+          i) A Borrower can have 0 or more Invoices.
           <br></br>
-          ii) Only approved invoices can be purchased.
+          ii) Only created Invoices can be approved or rejected.
           <br></br>
-          iii) Only purchased invoices can be closed.
+          iii) Only approved Invoices can be purchased.
+          <br></br>
+          iv) Only purchased Invoices can be closed.
         </p>
         <hr className="my-4" />
         <span>
           <Link
-            to="/invoice"
+            to="/borrower"
             className="btn btn-lg custom-button"
             role="button"
           >
-            Create Invoice
+            Create Borrower
           </Link>
           <Link
-            to="/invoices"
+            to="/borrowers"
             className="btn btn-lg custom-button"
             role="button"
           >
-            View Invoices
+            View Borrowers
           </Link>
         </span>
       </div>
